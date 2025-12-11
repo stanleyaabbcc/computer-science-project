a3664d0760b808b616305f0e8a1dad80e7fb93c9
stanley <stantu24@gmail.com>
2025-12-12 00:24:46 +0800

    1:18:43

--- game.py
+++ game.py
@@ -1,14 +1,17 @@
 import pygame
 import random
+import os
 
 FPS = 60
 WIDTH = 500
 HEIGHT = 600
 
 GREEN = (0,255,0)
+REALWHITE = (255,255,255)
 WHITE = (253,245,230)
 RED = (255,0,0)
 YELLOW = (255,255,0)
+BLACK = (0,0,0)
 
 #遊戲初始化,遊戲視窗
 pygame.init()
@@ -17,12 +20,20 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("計概專題遊戲") #視窗命名
 running = True
 
+#載入圖片
+player_img = pygame.image.load(os.path.join("img","player.png")).convert()
+rock_img = pygame.image.load(os.path.join("img","rock.png")).convert()
+bullet_img = pygame.image.load(os.path.join("img","bullet.png")).convert()
+boss_img = pygame.image.load(os.path.join("img","boss.png")).convert()
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50,40))
-        self.image.fill(GREEN)
+        self.image = pygame.transform.scale(player_img,(43,65))
+        self.image.set_colorkey(REALWHITE)
         self.rect = self.image.get_rect() #定位物件
+        self.radius = 21
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 8
@@ -40,13 +51,15 @@ class Player(pygame.sprite.Sprite):
     def shoot(self):    
         bullet = Bullet(self.rect.centerx,self.rect.top)
         all_sprites.add(bullet)    
+        bullets.add(bullet)
 
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30,40))
-        self.image.fill(RED)
+        self.image = pygame.transform.scale(rock_img,(65,59))
+        self.image.set_colorkey(REALWHITE)
         self.rect = self.image.get_rect() #定位物件
+        self.radius = 25
         self.rect.x = random.randrange(0 , WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100 , -40)
         self.speedy = random.randrange(2, 10)
@@ -63,8 +76,8 @@ class Rock(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10,20))
-        self.image.fill(YELLOW)
+        self.image = pygame.transform.scale(bullet_img,(18,34))
+        self.image.set_colorkey(REALWHITE)
         self.rect = self.image.get_rect() #定位物件
         self.rect.centerx = x
         self.rect.bottom = y
@@ -76,11 +89,14 @@ class Bullet(pygame.sprite.Sprite):
             self.kill()
 
 all_sprites = pygame.sprite.Group()
+rocks = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 for i in range(8):
     r = Rock()
     all_sprites.add(r)
+    rocks.add(r)
 
 #建立遊戲迴圈
 while running:
@@ -95,6 +111,15 @@ while running:
     #更新遊戲
 
     all_sprites.update()
+    hits = pygame.sprite.groupcollide(rocks,bullets,True,True) #true判斷前面的rocks和bullets碰撞後要不要刪除
+    for hit in hits:
+        r = Rock()
+        all_sprites.add(r)
+        rocks.add(r)
+
+    hits = pygame.sprite.spritecollide(player,rocks,False,pygame.sprite.collide_circle)    
+    if hits:
+        running = False
 
     #畫面顯示
     screen.fill(WHITE) #改變遊戲顏色(後面是rgb值)
